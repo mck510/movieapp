2b041b1ae7020636d0bcbbfbc94ed4ffec88403a
Mathias Kronemann <mathias.kronemann@stud.fh-campuswien.ac.at>
2022-04-01 12:37:36 +0200

    test

--- app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
@@ -40,7 +40,7 @@ fun MainContent(content: @Composable () -> Unit){
                     Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "ArrowBack",
                     modifier = Modifier.clickable {
                         //TODO goBack
-                        //test
+                        //test2
                     })
 
                     Spacer(modifier = Modifier.width(28.dp))
